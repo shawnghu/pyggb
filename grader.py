eebefa00ed2ea04f53e6b07c029c214558dcd0be
Shawn Hu <shawnghu@gmail.com>
2025-04-25 02:37:06 -0700

    vLLM optimizations to the grader
    found that openthinker was not using kv cache
    
    attempting to optimize more will probably not work.
    because openthinker is a reasoning model, its sequences are long, and
    runtime is dominated by attention calculation, not prefill, and
    attention calculation is already maximally optimized by vllm, using
    flashattention 3 and selecting the best kernel for the architecture
    automatically.
    
    see dev notes.

--- grader.py
+++ grader.py
@@ -1,12 +1,15 @@
 import os
+# os.environ["VLLM_ATTENTION_BACKEND"] = "FLASHINFER"
+# os.environ["VLLM_FLASHINFER_FORCE_TENSOR_CORES"] = "1" # https://github.com/vllm-project/vllm/issues/9471 ; still slower than simply not using flashinfer for attention
 import argparse
 import json
 import torch
 from validator import extract_last_float, isclose, floatify
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import copy
-
+import pdb
 from vllm import LLM, SamplingParams
+# from vllm.engine.arg_utils import HfOverrides
 
 qwen_model_name = "Qwen/Qwen2.5-32B-Instruct"
 openthinker_model_name = "open-thoughts/OpenThinker-32B"
@@ -22,7 +25,11 @@ def main():
     parser.add_argument("--tensor_parallel_size", type=int, default=1)
     parser.add_argument("--pipeline_parallel_size", type=int, default=1)
     args = parser.parse_args()
+    main_with_args(args)
+    args.model_shortname = "openthinker"
+    main_with_args(args)
 
+def main_with_args(args):
     if args.model_shortname == "qwen":
         model_name = qwen_model_name
     elif args.model_shortname == "openthinker":
@@ -40,14 +47,19 @@ def main():
             args.input_file = args.input_file.replace(".jsonl", "_graded_easy.jsonl")
             output_file = args.input_file.replace(".jsonl", "_graded_medium.jsonl")
         
-
+    hf_overrides = {
+        "use_cache": True
+    }
     # Load model with vLLM - uses tensor parallelism automatically
     llm = LLM(
         model=model_name,
         tensor_parallel_size=args.tensor_parallel_size,
         pipeline_parallel_size=args.pipeline_parallel_size,
-        trust_remote_code=True,
-        dtype="float16"
+        trust_remote_code=False,
+        dtype="float16",
+        hf_overrides=hf_overrides,
+        # enable_chunked_prefill=True,
+        # max_num_batched_tokens=8192,
     )
     
     # Prepare all prompts at once for batching
